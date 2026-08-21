c3f018d794a82d5aef94938e8a5d35bb71402010
tarasska <taras.skazhenik@yandex.ru>
2026-08-21 21:44:28 +0300

    Add opts

---
+++ java/src/tskazhenik/ds/flatcombining/FcStackOpt8.java
@@ -0,0 +1,190 @@
+package tskazhenik.ds.flatcombining;
+
+import contention.abstractions.CompositionalQueue;
+import contention.abstractions.FlatCombiningStructure;
+import contention.benchmark.statistic.custom.FcStat;
+import tskazhenik.GlobalConstants;
+import tskazhenik.GlobalScopedValues;
+import tskazhenik.util.TTASLock;
+
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+import java.util.ArrayDeque;
+
+public class FcStackOpt8 extends FlatCombiningStructure implements CompositionalQueue<Integer> {
+    private static final int FC_ATTEMPTS = 8;
+    private static final int FC_THRESHOLD = 2;
+
+    @jdk.internal.vm.annotation.Contended
+    private final TTASLock lock;
+
+    @jdk.internal.vm.annotation.Contended
+    private final FcRequest[] fcRequestSlots;
+
+    @jdk.internal.vm.annotation.Contended
+    private final ArrayDeque<Integer> stack;
+
+    public FcStackOpt8() {
+        this.lock = new TTASLock();
+        this.fcRequestSlots = new FcRequest[GlobalConstants.THREADS_LIMIT];
+        this.stack = new ArrayDeque<>(100_000);
+
+        for (int i = 0; i < GlobalConstants.THREADS_LIMIT; i++) {
+            fcRequestSlots[i] = new FcRequest();
+        }
+    }
+
+    @Override
+    public boolean push(Integer value) {
+        var req = fcRequestSlots[GlobalScopedValues.THREAD_ID.get()];
+        req.value = value;
+        req.type = FCOperationType.PUSH;
+
+        handleRequest(req);
+
+        return req.value != null;
+    }
+
+    @Override
+    public Integer pop() {
+        var req = fcRequestSlots[GlobalScopedValues.THREAD_ID.get()];
+        req.value = null;
+        req.type = FCOperationType.POP;
+
+        handleRequest(req);
+
+        return req.value;
+    }
+
+    @Override
+    public boolean contains(Integer value) {
+        var req = fcRequestSlots[GlobalScopedValues.THREAD_ID.get()];
+        req.value = value;
+        req.type = FCOperationType.CONTAINS;
+
+        handleRequest(req);
+
+        return req.value != null;
+    }
+
+    @Override
+    public int size() {
+        return stack.size();
+    }
+
+    @Override
+    public void clear() {
+    }
+
+    private void handleRequest(FcRequest req) {
+        req.publish();
+
+        while (true) {
+            if (lock.tryLock()) {
+                try {
+                    combine();
+                } finally {
+                    lock.unlock();
+                }
+                return;
+            } else {
+                while (req.isPublished() && lock.isLocked()) {
+                    Thread.yield();
+                }
+
+                if (!req.isPublished()) {
+                    return;
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void combine(FcStat stats) {
+        var registeredThreadsCount = GlobalScopedValues.MAX_THREADS.get();
+        for (int t = 0; t < FC_ATTEMPTS; ++t) {
+            int ops = 0;
+            for (int i = 0; i < registeredThreadsCount; i++) {
+                var req = fcRequestSlots[i];
+
+                if (req.isPublished()) {
+                    ++ops;
+
+                    if (req.type == FCOperationType.PUSH) {
+
+                        stack.push(req.value);
+                    } else if (req.type == FCOperationType.POP) {
+
+                        if (stack.isEmpty()) {
+                            req.value = null;
+                        } else {
+                            req.value = stack.pop();
+                        }
+                    } else if (req.type == FCOperationType.CONTAINS) {
+
+                        if (!stack.contains(req.value)) {
+                            req.value = null;
+                        }
+                    }
+
+                    req.markFinished();
+                }
+            }
+
+            stats.ops += ops;
+            if (ops < FC_THRESHOLD) {
+                stats.attempts += t + 1;
+                return;  // not enough pending work
+            }
+        }
+    }
+
+    enum FCOperationType {
+        NONE,
+        PUSH,
+        POP,
+        CONTAINS
+    }
+
+    enum FCStatus  {
+        EMPTY ,
+        PUSHED,
+        FINISHED
+    }
+
+    @jdk.internal.vm.annotation.Contended
+    private static class FcRequest {
+        @jdk.internal.vm.annotation.Contended
+        FCOperationType type = FCOperationType.NONE;
+
+        @jdk.internal.vm.annotation.Contended
+        Integer value = 0;
+
+        @jdk.internal.vm.annotation.Contended
+        boolean published = false;
+
+        public void publish() {
+            PUBLISHED_HANDLE.setRelease(this, true);
+        }
+
+        public void markFinished() {
+            PUBLISHED_HANDLE.setRelease(this, false);
+        }
+
+        public boolean isPublished() {
+            return (boolean) PUBLISHED_HANDLE.getAcquire(this);
+        }
+
+        private static final VarHandle PUBLISHED_HANDLE;
+
+        static {
+            try {
+                // Find an instance field VarHandle using Lookup
+                PUBLISHED_HANDLE = MethodHandles.lookup()
+                        .findVarHandle(FcRequest.class, "published", boolean.class);
+            } catch (ReflectiveOperationException e) {
+                throw new Error(e);
+            }
+        }
+    }
+}
---
+++ java/src/tskazhenik/ds/flatcombining/FcStackOptFence.java
@@ -0,0 +1,164 @@
+package tskazhenik.ds.flatcombining;
+
+import contention.abstractions.CompositionalQueue;
+import contention.abstractions.FlatCombiningStructure;
+import contention.benchmark.statistic.custom.FcStat;
+import tskazhenik.GlobalConstants;
+import tskazhenik.GlobalScopedValues;
+import tskazhenik.util.TTASLock;
+
+import java.lang.invoke.VarHandle;
+import java.util.ArrayDeque;
+
+public class FcStackOptFence extends FlatCombiningStructure implements CompositionalQueue<Integer>  {
+    private static final int FC_ATTEMPTS = 16;
+    private static final int FC_THRESHOLD = 2;
+
+    @jdk.internal.vm.annotation.Contended
+    private final TTASLock lock;
+
+    @jdk.internal.vm.annotation.Contended
+    private final FcRequest[] fcRequestSlots;
+
+    @jdk.internal.vm.annotation.Contended
+    private final ArrayDeque<Integer> stack;
+
+    public FcStackOptFence() {
+        this.lock = new TTASLock();
+        this.fcRequestSlots = new FcRequest[GlobalConstants.THREADS_LIMIT];
+        this.stack = new ArrayDeque<>(100_000);
+
+        for (int i = 0; i < GlobalConstants.THREADS_LIMIT; i++) {
+            fcRequestSlots[i] = new FcRequest();
+        }
+    }
+
+    @Override
+    public boolean push(Integer value) {
+        var req = fcRequestSlots[GlobalScopedValues.THREAD_ID.get()];
+        req.value = value;
+        req.type = FCOperationType.PUSH;
+
+        handleRequest(req);
+
+        return req.value != null;
+    }
+
+    @Override
+    public Integer pop() {
+        var req = fcRequestSlots[GlobalScopedValues.THREAD_ID.get()];
+        req.value = null;
+        req.type = FCOperationType.POP;
+
+        handleRequest(req);
+
+        return req.value;
+    }
+
+    @Override
+    public boolean contains(Integer value) {
+        var req = fcRequestSlots[GlobalScopedValues.THREAD_ID.get()];
+        req.value = value;
+        req.type = FCOperationType.CONTAINS;
+
+        handleRequest(req);
+
+        return req.value != null;
+    }
+
+    @Override
+    public int size() {
+        return stack.size();
+    }
+
+    @Override
+    public void clear() {
+    }
+
+    private void handleRequest(FcRequest req) {
+        req.published = true;
+        VarHandle.releaseFence();
+
+        while (true) {
+            if (lock.tryLock()) {
+                try {
+                    combine();
+                } finally {
+                    lock.unlock();
+                }
+                return;
+            } else {
+                while (req.published && lock.isLocked()) {
+                    Thread.yield();
+                    VarHandle.acquireFence();
+                }
+
+                if (!req.published) {
+                    return;
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void combine(FcStat stats) {
+        var registeredThreadsCount = GlobalScopedValues.MAX_THREADS.get();
+        for (int t = 0; t < FC_ATTEMPTS; ++t) {
+            VarHandle.acquireFence();
+            int ops = 0;
+            for (int i = 0; i < registeredThreadsCount; i++) {
+                var req = fcRequestSlots[i];
+
+                if (req.published) {
+                    ++ops;
+
+                    if (req.type == FCOperationType.PUSH) {
+
+                        stack.push(req.value);
+                    } else if (req.type == FCOperationType.POP) {
+
+                        if (stack.isEmpty()) {
+                            req.value = null;
+                        } else {
+                            req.value = stack.pop();
+                        }
+                    } else if (req.type == FCOperationType.CONTAINS) {
+
+                        if (!stack.contains(req.value)) {
+                            req.value = null;
+                        }
+                    }
+
+                    req.published = false;
+                }
+                VarHandle.releaseFence();
+            }
+
+            stats.ops += ops;
+            if (ops < FC_THRESHOLD) {
+                stats.attempts += t + 1;
+                return;  // not enough pending work
+            }
+        }
+    }
+
+    enum FCOperationType {
+        NONE,
+        PUSH,
+        POP,
+        CONTAINS
+    }
+
+    @jdk.internal.vm.annotation.Contended
+    private static class FcRequest {
+        @jdk.internal.vm.annotation.Contended("d")
+        FCOperationType type = FCOperationType.NONE;
+
+        @jdk.internal.vm.annotation.Contended("d")
+        Integer value = 0;
+
+        @jdk.internal.vm.annotation.Contended("s")
+        boolean published = false;
+
+    }
+}
